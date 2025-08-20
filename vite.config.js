685ee94f05381835902bdbe5ec5c7a16e3f424ca
waqasansariii <159879624+waqasansariii@users.noreply.github.com>
2025-08-20 09:17:36 +0500

    latest commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),cloudflare({
+    wrangler: {
+      compatibilityDate: '2025-08-19',
+    },})],
 })
+
